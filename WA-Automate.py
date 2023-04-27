57a7de16a85d55a559644a430886abc5d099965e
Satyam Verma <satyamverma46@outlook.com>
2023-04-27 21:02:24 +0530

    Safe Mode Disabled of PyAutoGUI

--- WA-Automate.py
+++ WA-Automate.py
@@ -1,6 +1,7 @@
 import webbrowser
-import pyautogui as pyAutoGUI
+import pyautogui as PyAutoGUI
 import time
+PyAutoGUI.FAILSAFE = False
 mobNum = input("Enter the mobile number to message:- ")
 msgLimit = int(input("Enter the number of messages you want to send:- "))
 msgText = input("Enter the message you want to send:- ")
@@ -8,7 +9,7 @@ webbrowser.open(f"https://web.whatsapp.com/send?phone={mobNum}")
 i = 0
 time.sleep(30)
 while i < msgLimit:
-    pyAutoGUI.write(msgText, interval=0)
-    pyAutoGUI.press("enter")
+    PyAutoGUI.write(msgText, interval=0)
+    PyAutoGUI.press("enter")
     i += 1
 time.sleep(5)
